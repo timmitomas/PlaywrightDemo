1825f3841fecf54013c2f225498a9f457a0e5e71
timmitomas <timmitomas@gmail.com>
2024-08-20 11:37:00 +0300

    Init
    Init

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,3 +1,5 @@
+package org.example;
+
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.AriaRole;
 
--- src/main/java/org/example/TestFixtures.java
+++ src/main/java/org/example/TestFixtures.java
@@ -1,3 +1,5 @@
+package org.example;
+
 import com.microsoft.playwright.*;
 import org.junit.jupiter.api.*;
 
